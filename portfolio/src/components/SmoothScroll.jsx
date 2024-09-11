add4fc34cd0746895480db2fcc8a22ef39d9b3bf
Shhmon <Siizmonsus@gmail.com>
2024-09-11 17:03:25 +0200

    FEAT: Implement Tech Stack Visual animations. FIX: Temporary fixes to some overflow issues with animations.

--- portfolio/src/components/SmoothScroll.jsx
+++ portfolio/src/components/SmoothScroll.jsx
@@ -1,64 +1,35 @@
 import { useRef } from 'react';
 import { motion, useScroll, useTransform } from 'framer-motion';
+import { FaReact, FaHtml5, FaCss3Alt, FaNodeJs, FaJs } from "react-icons/fa6"
 
 export const SmoothScroll = () => {
     return (
-    <div className="bg-MainLight"> 
-        <Hero />
-        <div className="h-screen" />
-    </div>
+        <div className="bg-MainLight">
+            <Hero />
+            <div className="h-screen" />
+        </div>
     );
 };
 
-// const Nav = () => {
-//     return (
-//         <nav className="fixed left-0 right-0 top-0 z-50
-//         flex items-center justify-between px-6 py-3 text-MainLight">
-//             <h1 className="font-Anton 3xl mix-blend-difference">Szymon Samus Dev.</h1>
-//             <button
-//             onClick={() => {
-//                 document.getElementById("hero")?.
-//                  scrollIntoView({
-//                     behavior: 'smooth',
-//                  });
-//             }}
-//             className="flex items-center gap-1 text-xs text-MainLight"
-//             >
-//                 Proceed. 
-//             </button>
-//         </nav>
-//     );
-// };
-
-const SECTION_HEIGHT = 1000;
+const SECTION_HEIGHT = 1500;
 
 const Hero = () => {
     return (
-        <div 
-        className="relative w-full bg-MainDark"
-        style={{ height: `calc(${SECTION_HEIGHT}px + 100vh)`}}>
+        <div
+            className="relative w-full bg-MainDark overflow-x-hidden"
+            style={{ height: `calc(${SECTION_HEIGHT}px + 100vh)` }}>
             <h1 className='text-MainLight font-WorkSans uppercase text-4xl text-center pt-80 m-auto'>Szymon Samus</h1>
-            <h2 className='text-MainLight font-Anton uppercase text-5xl text-center m-auto pb-[500px]'>Frontend/Fullstack Developer</h2>
+            <h2 className='text-MainLight font-Anton uppercase text-5xl text-center m-auto pb-[400px]'>Frontend Developer</h2>
             <ParallaxAbout />
+            <ParallaxTechStack />
             <div className='absolute bottom-0 left-0 right-0
-            h-96 bg-gradient-to-b from from-MainLight/0 to-MainLight' 
+            h-96 bg-gradient-to-b from from-MainLight/0 to-MainLight'
             />
         </div>
     )
 };
 
-// const CenterImage = () => {
-//     return (
-//         <div 
-//         className="sticky top-0 h-screen w-full"
-//         style={{
-//             backgroundImage: `url(${Background})`,
-//             backgroundPosition: 'center',
-//             backgroundRepeat: 'no-repeat',
-//         }}
-//         />
-//     )
-// }
+// ABOUT INTRO
 
 const ParallaxAbout = () => {
     const targetRef = useRef(null);
@@ -67,49 +38,56 @@ const ParallaxAbout = () => {
     });
 
     return (
-    <motion.div 
-    className='flex flex-col items-center mx-auto max-w-5xl px-4'
-    ref={targetRef}
-    initial={{
-        x: 1000,
-    }}
-    whileInView={{
-        x: 0,
-    }}
-    transition={{duration: 3}}
-    >
-        <ParallaxHeader
-        text="About"
-        className="uppercase font-Anton text-MainLight text-5xl"
-        />
-        <ParallaxDescription 
-        text="My name is Szymon, I am a Frontend Developer with a passion for bringing sleek user interfaces to life."
-        className="font-WorkSans text-MainLight text-2xl text-center"
-        />
-    </motion.div>
-);
+        <motion.div
+            className='flex flex-col items-center mx-auto max-w-5xl px-4 pb-[300px]'
+        >
+            <ParallaxHeader
+                text="About"
+                className="uppercase font-Anton text-MainLight text-5xl"
+            />
+            <ParallaxDescription
+                text="I am a Junior Frontend Developer with a passion for bringing sleek and user-friendly interfaces to life. Constantly staying updated on the latest trends and eager to contribute to innovative projects."
+                className="font-WorkSans text-MainLight text-2xl text-center"
+            />
+        </motion.div>
+    );
 }
 
 const ParallaxHeader = ({
     text,
     className,
     start,
-    end,  
+    end,
 }) => {
     const { scrollY } = useScroll();
 
     const opacity = useTransform(
         scrollY,
-        [0, 500],
+        [100, 500],
         [0, 1])
     return (
         <motion.header
-        className={className}
-        style={{
-            opacity,
-        }}
+            className={className}
+            style={{
+                opacity,
+            }}
+            initial={{
+                x: 600,
+            }}
+            whileInView={{
+                x: 0,
+            }}
+            transition={{
+                duration: 3,
+                type: "spring",
+                stiffness: 50,
+            }}
+            viewport={{
+                margin: '-150px',
+                once: true,
+            }}
         >
-        {text}
+            {text}
         </motion.header>
     )
 }
@@ -118,23 +96,183 @@ const ParallaxDescription = ({
     text,
     className,
     start,
-    end,  
+    end,
 }) => {
     const { scrollY } = useScroll();
 
     const opacity = useTransform(
         scrollY,
-        [0, 500],
+        [100, 500],
         [0, 1]
     )
     return (
         <motion.span
-        className={className}
-        style={{
-            opacity,
-        }}
+            className={className}
+            style={{
+                opacity,
+            }}
+            initial={{
+                x: -750,
+            }}
+            whileInView={{
+                x: 0,
+            }}
+            transition={{
+                duration: 3,
+                type: "spring",
+                stiffness: 100,
+            }}
+            viewport={{
+                once: true,
+            }}
+
         >
-        {text}
+            {text}
+        </motion.span>
+    )
+}
+
+// TECH STACK
+
+const gridVariants = {
+    hidden: {
+        opacity: 0
+    },
+    show: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.4,
+        },
+    },
+};
+
+const gridItemVariants = {
+    hidden: {
+        opacity: 0,
+        y: 40,
+    },
+    show: {
+        opacity: 1,
+        y:0,
+        transition: {
+            duration: 0.3,
+        },
+    },
+};
+
+const ParallaxTechStack = () => {
+    return (
+        <motion.div
+            className="flex flex-col items-center justify-center mx-auto px-4"
+        >
+            {/* MAIN TECHNOLOGIES DISPLAY */}
+            <motion.section className='flex flex-col justify-center text-center p-24'>
+                <ParallaxTechHeader
+                    text="My Technologies"
+                    className="font-Anton text-MainLight uppercase text-6xl tracking-wider pb-10"
+                    initial={{
+                        x: -500,
+                        opacity: 0,
+                    }}
+                    whileInView={{
+                        x: 0,
+                        opacity: 1,
+                    }}
+                />
+                <motion.div
+                    variants={gridVariants}
+                    initial="hidden"
+                    whileInView="show"
+                    className='grid grid-cols-5 gap-4'
+                >
+                    <motion.span
+                        variants={gridItemVariants}
+                        className='text-7xl text-MainLight flex justify-center items-center'
+                    >
+                        <FaHtml5 />
+                    </motion.span>
+                    <motion.span
+                        variants={gridItemVariants}
+                        className='text-7xl text-MainLight'
+                    >
+                        <FaCss3Alt />
+                    </motion.span>
+                    <motion.span
+                        variants={gridItemVariants}
+                        className='text-7xl text-MainLight'
+                    >
+                        <FaJs />
+                    </motion.span>
+                    <motion.span
+                        variants={gridItemVariants}
+                        className='text-7xl text-MainLight'
+                    >
+                        <FaReact />
+                    </motion.span>
+                    <motion.span
+                        variants={gridItemVariants}
+                        className='text-7xl text-MainLight'
+                    >
+                        <FaNodeJs />
+                    </motion.span>
+                </motion.div>
+            </motion.section>
+        </motion.div>
+    )
+}
+
+const ParallaxTechHeader = ({
+    className,
+    text,
+    initial,
+    whileInView
+}) => {
+    return (
+        <>
+            <motion.header
+                className={className}
+                initial={initial}
+                whileInView={whileInView}
+                transition={{
+                    duration: 3,
+                    type: "spring",
+                    stiffness: 35,
+                }}
+                viewport={{
+                    margin: '-150px',
+                    once: true,
+                }}
+            >
+                {text}
+
+                {/* <FaHtml5 />
+
+<FaReact />
+
+<FaCss3Alt />
+
+<FaNodeJs /> */}
+            </motion.header>
+        </>
+    )
+}
+
+const ParallaxIcon = ({
+    className,
+    text,
+    initial,
+    whileInView,
+    transition
+}) => {
+    return (
+        <motion.span
+            className={className}
+            text={text}
+            initial={initial}
+            whileInView={whileInView}
+            transition={transition}
+        >
+            {text}
         </motion.span>
     )
 }
